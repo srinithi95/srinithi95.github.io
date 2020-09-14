5dd5090329b10d091d8eaf57f09808a4849bd0bb
srinithi95 <60716676+srinithi95@users.noreply.github.com>
2020-09-13 19:37:45 -0700

    Updates

---
+++ precache-manifest.418070708511ee27ff81ef5d538e124b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65c4ba245ce4539f6f67",
+    "url": "/me/static/css/main.eba5507e.chunk.css"
+  },
+  {
+    "revision": "65c4ba245ce4539f6f67",
+    "url": "/me/static/js/main.65c4ba24.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "148cfc4877a54e57ea80ca701d676067",
+    "url": "/me/index.html"
+  }
+];
